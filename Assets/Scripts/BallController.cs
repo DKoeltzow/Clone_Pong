e3e173758effa04084573062fbff256a821265c0
DKoeltzow <d.koeltzow@web.de>
2018-03-15 11:41:48 +0100

    first steps on screen

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    Vector2 pos;
+    // Use this for initialization
+    void Start () {
+      pos = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(0,0,0)).x, Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.position= pos;
+		
+	}
+}
